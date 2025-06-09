d6d1d4a12883e3aa63a8ae6db9e4d578591b96ce
bhaumikmaan <bhaumikmaan@gmail.com>
2025-06-09 12:46:24 +0530

    Application Init and list topics

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"go-kafka-application/producer"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	arg := os.Args[1:]
+	if len(arg) == 0 {
+		menu()
+	} else {
+		action := arg[0]
+		switch action {
+		case "close":
+			fmt.Println("Closing the applcation! Bye.")
+			os.Exit(0)
+		case "list-topics":
+			producer.PrintTopics()
+		default:
+			fmt.Println("Unknown action. Please input again.")
+			menu()
+		}
+	}
+}
+
+func menu() {
+	fmt.Println("Welcome to the Go-Kafka Application! Please enter the argument for the action you want to perform")
+	fmt.Println("1. list-topics - Lists all the topics\n" +
+		"2. start-producer - Reads the stream and produces in topic\n" +
+		"3. stop-producer - Stops reading from the stream\n" +
+		"4. start-consumer - Consumes a new topic\n" +
+		"5. stop - Closes the application")
 }
--- producer/main.go
+++ producer/main.go
@@ -1,7 +1,38 @@
-package main
+package producer
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/segmentio/kafka-go"
+)
 
 func main() {
 	fmt.Println("Hello Producer")
+}
+
+// Exported Functions start with a capitalized Letter
+func PrintTopics() {
+	// Connect to localhost:9092
+	conn, err := kafka.Dial("tcp", "localhost:9092")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer conn.Close()
+
+	// Read Partitions
+	partitions, err := conn.ReadPartitions()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	m := map[string]struct{}{}
+
+	// Map the partitions with the topic as the key
+	for _, p := range partitions {
+		m[p.Topic] = struct{}{}
+	}
+
+	// Print the topics
+	for k := range m {
+		fmt.Println(k)
+	}
 }
